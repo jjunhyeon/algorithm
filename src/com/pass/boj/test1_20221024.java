5c9c9332812c0c61f63e0db69a91633289af58a8
김준현 <sonjs01@naver.com>
2022-11-10 00:24:27 +0900

    first commit

---
+++ src/com/pass/boj/test1_20221024.java
@@ -0,0 +1,17 @@
+package com.pass.boj;
+
+import java.util.Scanner;
+
+public class test1_20221024 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("찍을 *의 수를 입력하세요");
+        int n = sc.nextInt();
+        for(int i=0; i<n; i++){
+            for(int j=n-i; j<=n; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
