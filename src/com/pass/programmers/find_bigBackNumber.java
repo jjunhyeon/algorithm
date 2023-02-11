56b98a274ffd99d58c3262b12e1eb6f501e210c9
sonjs01@naver.com <sonjs01@naver.com>
2023-02-11 16:38:46 +0900

    프로그래머스 뒷큰수 찾기 효율성 개선

--- src/com/pass/programmers/find_bigBackNumber.java
+++ src/com/pass/programmers/find_bigBackNumber.java
@@ -14,30 +14,21 @@ public class find_bigBackNumber {
     // 문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/154539?language=java
     private static int[] Solution(int[] numbers) {
         
-        // 최종 정답 배열 변수
-        int[] answer;
-        // 배열에 값을 추가하기 위한 리스트
-        List<Integer> tmpArrayList = new ArrayList<>();
+        // answer 배열을 파라미터 numbers 크기로 초기화
+        int[] answer = new int[numbers.length];
 
-        // 배열의 index를 기준으로 순회하면서 큰 수가 존재하는지 확인한다.
-        for (int t = 0; t <= numbers.length; t++) {
-            if (t == numbers.length) {
-                tmpArrayList.add(-1);
-                break;
-            }
+        // 배열의 index를 기준으로 순회하면서 -1로 세팅
+        for (int t = 0; t < numbers.length; t++) {
+            answer[t] = -1;
             for (int j = t + 1; j < numbers.length; j++) {
-                // 뒷큰수가 존재한다면 리스트에 갑을 넣고 break
+                // 뒷큰수가 존재한다면 값 바꾸고 break;
                 if (numbers[t] < numbers[j]) {
-                    tmpArrayList.add(numbers[j]);
+                    answer[t] = numbers[j];
                     break;
-                    // 마지막 순회할때까지 큰 값이 없다면 리스트 -1 추가
-                } else if (j == numbers.length - 1 && numbers[t] >= numbers[j]) {
-                    tmpArrayList.add(-1);
                 }
             }
         }
-        // list to Array 리턴
-        return tmpArrayList.stream().mapToInt(Integer::intValue).toArray();
+        return answer;
     }
 }
 
