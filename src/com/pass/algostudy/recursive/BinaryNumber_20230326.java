0171bc70a2be31aab296d565126c5faf89928365
김준현 <sonjs01@naver.com>
2023-04-02 22:53:01 +0900

    이진수출력, 피보나치, 재귀, 팩토리얼 등 메모이제이션 다시 볼 것

---
+++ src/com/pass/algostudy/recursive/BinaryNumber_20230326.java
@@ -0,0 +1,25 @@
+package com.pass.algostudy.recursive;
+
+/*
+* 이진수 출력(재귀로)
+* */
+public class BinaryNumber_20230326 {
+    static class Main {
+        public void DFS(int n) {
+
+            if(n==0){
+                return;
+            } else{
+                DFS(n/2);
+                System.out.print(n%2 + " ");
+            }
+        }
+    }
+    /*
+     * 값 입력받기
+     * */
+    public static void main(String[] args) {
+        Main T = new Main();
+        T.DFS(11);
+    }
+}
