8f518acc64d612f193b975d45d64b7d679b8278e
김준현 <sonjs01@naver.com>
2023-04-06 00:13:05 +0900

    정렬 - 선택정렬 (무난)

--- src/com/pass/algostudy/sorting/SelectionSort_20230405.java
+++ src/com/pass/algostudy/sorting/SelectionSort_20230405.java
@@ -1,7 +1,11 @@
 package com.pass.algostudy.sorting;
 
 import java.util.Scanner;
-
+/*
+* 선택정렬
+* 입력받은 값을 반복문을 통해 탐색하며
+* 더 작은값이 있을 경우 값의 위치를 바꿔 정렬하는 문제
+* */
 public class SelectionSort_20230405 {
     // 실제 Solution
     static class Main {
@@ -10,14 +14,17 @@ public class SelectionSort_20230405 {
             for(int i=0; i<array.length; i++){
                 int tmp = i;
                 for(int j=i+1; j<n; j++){
+                    // 더 작은 idx값 저장 최종 tmp는 array[i]위치에 들어갈 값
                     if(array[tmp] > array[j]){
                         tmp = j;
                     }
                 }
+                // 값 위치 바꾸기
                 int t = array[tmp];
                 array[tmp] = array[i];
                 array[i] = t;
-
+                
+                // 결과를 바로바로 result에 추가한다
                 result += array[i] + " ";
             }
             return result;
