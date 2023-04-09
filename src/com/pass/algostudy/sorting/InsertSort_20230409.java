3cf142a12fbbefeeebabe53c3033352eed4c1b12
sonjs01@naver.com <sonjs01@naver.com>
2023-04-09 19:18:19 +0900

    정렬 - 삽입정렬(보통)

--- src/com/pass/algostudy/sorting/InsertSort_20230409.java
+++ src/com/pass/algostudy/sorting/InsertSort_20230409.java
@@ -9,13 +9,30 @@ import java.util.Scanner;
 * 단점 : 시간 복잡도가 O(N^2)로 비효율적입니다.
 * -> 선택 정렬, 버블 정렬, 삽입 정렬은 모두 시간 복잡도가 O(N^2)이므로, 대규모 데이터 정렬에는 비효율적이고
 * 이를 개선한 퀵 정렬, 병합 정렬 등의 알고리즘이 있습니다.
+* 1차원에 배열에 담긴 값을 정렬 할 때
+* 제일 처음 0번쨰 부터가 아닌 , i가 1부터 순회한다.
+*
 * */
 public class InsertSort_20230409 {
     // 실제 Solution
     static class Main {
         public String solution(int n, int[] array) {
             String result = "";
+            for(int i=1; i< array.length; i++){
+                int tmp = array[i],j;
+                for(j= i-1; j>-1; j--){
+                    if(array[j] > tmp){
+                        array[j+1] = array[j];
+                    } else {
+                        break;
+                    }
+                }
+                array[j+1] = tmp;
+            }
 
+            for(int i=0; i< array.length; i++){
+                result += array[i] + " ";
+            }
             return result;
         }
     }
