f79d489f57a949b76a4b3e331ce18758f44b44bb
sonjs01@naver.com <sonjs01@naver.com>
2023-05-01 14:51:46 +0900

    그래프 추가 (최단거리, 리스트 ,BFS) - 중요

--- src/com/pass/algostudy/graph/Graph_20230501.java
+++ src/com/pass/algostudy/graph/Graph_20230501.java
@@ -1,2 +1,64 @@
-package com.pass.algostudy.graph;public class Graph_20230501 {
+package com.pass.algostudy.graph;
+
+import com.sun.tools.javac.Main;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * 그래프
+ * G(V,E)
+ * Vertex와 Edge로 이루어져 있다.
+ * [1,2],[1,3][2,4],[2,5]... 등의 형식으로 인접행렬이 들어온다.
+ * 2차원 배열
+ * 간선정보가 2차원 배열안에 들어있다.
+ * graph[a][b] = 1; -> 연관 관계가 있다.
+ * graph[b][a] = 1; -> [1,2] 간서 정보 표현 방식
+ *
+ * 방향그래프(2차원 배열)
+ * 1 2 -> 1에서 2로 방향에 대한 정보
+ * 2 3 -> 2에서 3으로 가는 방향에 대한 정보
+ *
+ * 가중치 방향 그래프
+ * 방향 정보에 가중치 값이 들어있음
+ *
+ * */
+public class Graph_20230501 {
+    static int n,m, answer = 0;
+    static int[][] graph;
+    static int[] ch;
+    static class Main{
+        public void DFS(int V){
+            // v의 값은 node의 개수
+            if(V == n){
+                // 마지막 노드까지의 케이스를 찾음
+                answer++;
+            } else { // 1번 노드부터 그래프 정보를 탐색하면서 개수를 찾아간다.
+                for(int i=1; i<=n; i++){ // root부터 연결된 정보를 찾아가는데
+                    if(ch[i] == 0 && graph[V][i] == 1){ // 방문하지 않았는데 인접 정보가 있다면 찾아나감
+                        ch[i] = 1; // 해당 노드를 방문했음을 저장하고
+                        DFS(i); // 하위 노드를 호출한다. (DFS(1) -> DFS(2) 호출)
+                        ch[i] = 0; // 호출 이후 다시 방문정보를 0으로 지워준다.
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        n = kb.nextInt();
+        m = kb.nextInt();
+        ch = new int[n+1];
+        graph = new int[n+1][n+1];
+        for(int i=0; i<m; i++){
+            int a= kb.nextInt();
+            int b= kb.nextInt();
+            graph[a][b] = 1;
+        }
+        ch[1] = 1;
+        T.DFS(1);
+        System.out.println(answer);
+    }
 }
---
+++ src/com/pass/algostudy/graph/LinkedList_20230501.java
@@ -0,0 +1,58 @@
+package com.pass.algostudy.graph;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * 경로 탐색(인접리스트)
+ * 노드의 개수가 매우 많을 때 인접행렬로 풀기에 적합하지 않음
+ * 갈 수 있는 정점을 보기 위해선 노드의 개수만큼 돌기때문에
+ * 메모리 낭비가 심해짐
+ * 이를 해결 할 수 있는 방법 중 하나가 인접리스트를 사용하는 방법이다.
+ */
+public class LinkedList_20230501 {
+    static class Main {
+        static int n, m, answer = 0;
+        static ArrayList<ArrayList<Integer>> graph;
+        static int ch[];
+
+        public void LinkedList(int V) {
+
+            if (V == n) {
+                answer++;
+            } else {
+                // V번 ArrayList
+                for (int nv : graph.get(V)) {
+                    if (ch[nv] == 0) {
+                        ch[nv] = 1;
+                        LinkedList(nv);
+                        ch[nv] = 0;
+                    }
+                }
+            }
+        }
+
+        public static void main(String[] args) {
+            Main T = new Main();
+            Scanner kb = new Scanner(System.in);
+            n = kb.nextInt();
+            m = kb.nextInt();
+            ch = new int[n + 1];
+            graph = new ArrayList<ArrayList<Integer>>();
+            for (int i = 0; i <= n; i++) {
+                // 접근한값에 대해 null pointer 에러가 나오지 않도록 하기 위해 객체를 생성해야한다.
+                graph.add(new ArrayList<Integer>());
+            }
+
+            for (int i = 0; i < m; i++) {
+                int a = kb.nextInt();
+                int b = kb.nextInt();
+                graph.get(a).add(b);
+            }
+
+            ch[1] = 1;
+            T.LinkedList(1);
+            System.out.println(answer);
+        }
+    }
+}
