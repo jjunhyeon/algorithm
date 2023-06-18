c94bae8d9ddfeb5382942f4a8605fee3fef02031
김준현 <sonjs01@naver.com>
2023-06-18 13:51:36 +0900

    포켓몬마스터 - 사용자히 않는 import 삭제

--- src/com/pass/boj/Pokemon_1620_20230611.java
+++ src/com/pass/boj/Pokemon_1620_20230611.java
@@ -3,9 +3,7 @@ package com.pass.boj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Scanner;
 
 /*
  * 포켓몬마스터 이다솜
