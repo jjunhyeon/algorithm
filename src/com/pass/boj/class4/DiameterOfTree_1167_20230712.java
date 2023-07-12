62998f7e88fc9a6bc46d91669cfd2324c61d0392
김준현 <sonjs01@naver.com>
2023-07-12 23:57:38 +0900

    백준(1167번) 트리의지름(1차) - 시간초과 발생

---
+++ src/com/pass/boj/class4/DiameterOfTree_1167_20230712.java
@@ -0,0 +1,104 @@
+package com.pass.boj.class4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class DiameterOfTree_1167_20230712 {
+    // 그래프 정보
+    static ArrayList<ArrayList<Node>> nodeInfo = new ArrayList<ArrayList<Node>>();
+    // 정답 정보
+    static int[] answer;
+    public static class Node implements Comparable<Node>{
+        // 정점과 가중치 정보
+        int vertex, weight;
+        Node(int vertex, int weight){
+            this.vertex = vertex;
+            this.weight = weight;
+        }
+        @Override
+        public int compareTo(Node o) { // 가중치 값이 작은값 우선순위
+            return this.weight - o.weight;
+        }
+
+        @Override
+        public String toString(){
+            return this.vertex + " " + this.weight;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        // Node의 수
+        int N = Integer.parseInt(st.nextToken());
+        // 정답 N까지의 거리를 구하기 위함
+        int result = 0;
+        answer = new int[N+1];
+        for(int i=0; i<=N; i++){ // N번까지 값을 넣아야하므로 <= 처리
+            // edge에 Edge ArrayList 초기화
+            nodeInfo.add(new ArrayList<>());
+        }
+
+        while(N --> 0){
+            String[] lineNumbers = bf.readLine().split(" ");
+            // 0번은 정점 정보가 아닌 시작 노드, 마지막값은 의미없는값임
+            int from=-1,target=-1,weight = -1;
+            for(int i=0; i<lineNumbers.length-1; i++){
+                if(i ==0){ // 시작 노드
+                    from = Integer.parseInt(lineNumbers[i]);
+                } else if(i % 2 != 0){ // 타겟 노드
+                    target = Integer.parseInt(lineNumbers[i]);
+                } else { // 가중치
+                    weight = Integer.parseInt(lineNumbers[i]);
+                }
+                if(target != -1 && weight != -1){
+                    nodeInfo.get(from).add(new Node(target,weight));
+                    target = -1;
+                    weight = -1;
+                }
+            }
+        }
+        // 임의의 두점이므로 최대값을 구하면 됨
+        for(int i=1; i<nodeInfo.size(); i++){
+            int tmp = BFS(i);
+            result = Math.max(tmp,result);
+        }
+        System.out.println(result);
+    }
+
+    private static int BFS(int V) {
+        Arrays.fill(answer, Integer.MAX_VALUE);
+        int result = 0;
+
+        PriorityQueue<Node> nq = new PriorityQueue<>();
+        nq.offer(new Node(V,0));
+        while(!nq.isEmpty()){
+            Node target = nq.poll();
+            int nowVertex = target.vertex;
+            int nowWeight = target.weight;
+            // 1번 노드부터 시작
+            for(Node tn : nodeInfo.get(nowVertex)){
+                // 거리비교
+                if(answer[tn.vertex] > nowWeight + target.weight){
+                    // 최대 가중치로 업데이트 한다.
+                    nq.offer(new Node(tn.vertex,nowWeight + tn.weight));
+                    answer[tn.vertex] = tn.weight + nowWeight;
+                }
+            }
+        }
+
+        for(int i=1; i<answer.length; i++){
+            if(answer[i] == Integer.MAX_VALUE){
+                continue;
+            }
+            result = Math.max(result,answer[i]);
+        }
+        return result;
+    }
+}
