05c302160da4f7f9b7cf2dfda47a4c272415f4de
김준현 <37209763+jjunhyeon@users.noreply.github.com>
2023-07-15 21:36:27 +0900

    [Silver III] Title: 계단 오르기, Time: 136 ms, Memory: 13316 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int K = sc.nextInt();
+        int[] stair = new int[K+1];
+        int[] score = new int[K+1];
+        for(int i=1; i<=K; i++){
+            stair[i] = sc.nextInt();
+        }
+
+        if(K == 1){
+            System.out.println(stair[1]);
+        } else if(K==2){
+            System.out.println(stair[1] + stair[2]);
+        } else {
+            score[1] = stair[1];
+            score[2] = stair[1] + stair[2];
+            score[3] = Math.max(stair[1], stair[2]) + stair[3];
+            for(int i=4; i<=K; i++){
+                score[i] = Math.max(score[i-3] + stair[i-1], score[i-2])+ stair[i];
+            }
+            System.out.println(score[K]);
+        }
+    }
+}
