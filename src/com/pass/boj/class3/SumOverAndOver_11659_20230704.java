bbfd6328ce78771efd7977feedb933876e1ce2bb
김준현 <sonjs01@naver.com>
2023-07-15 21:21:34 +0900

    누적합 - 시간초과 해결

--- src/com/pass/boj/class3/SumOverAndOver_11659_20230704.java
+++ src/com/pass/boj/class3/SumOverAndOver_11659_20230704.java
@@ -5,8 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 /*
-* 누적합구하기
-* */
+ * 누적합구하기
+ * 시간초과
+ * */
 public class SumOverAndOver_11659_20230704 {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -14,22 +15,18 @@ public class SumOverAndOver_11659_20230704 {
         int N = Integer.parseInt(NM[0]);
         int M = Integer.parseInt(NM[1]);
 
-        int[] arr = new int[N+1];
+        int[] arr = new int[N + 1];
         String[] num = bf.readLine().split(" ");
-        for(int i=0; i<N; i++){
-            arr[i] = Integer.parseInt(num[i]);
+
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(num[i - 1]) + arr[i - 1];
         }
 
-        while(M --> 0){
-            int result = 0;
+        while (M-- > 0) {
             String[] nums = bf.readLine().split(" ");
             int from = Integer.parseInt(nums[0]);
             int to = Integer.parseInt(nums[1]);
-            for(int j=from-1; j<to; j++){
-                result += arr[j];
-            }
-            System.out.println(result);
+            System.out.println(arr[to] - arr[from - 1]);
         }
-
     }
 }
