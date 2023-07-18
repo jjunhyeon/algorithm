a31d5fd2b23e7656c622297a4582e7b44c749360
김준현 <sonjs01@naver.com>
2023-07-18 23:42:21 +0900

    계단오르기 - 다시풀어봐야함

---
+++ src/com/pass/boj/class3/Stair_20230715.java
@@ -0,0 +1,31 @@
+package com.pass.boj.class3;
+
+import java.util.Scanner;
+/*
+* 계단 오르기
+* */
+public class Stair_20230715 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int K = sc.nextInt();
+        int[] stair = new int[K+1];
+        int[] score = new int[K+1];
+        for(int i=1; i<=K; i++){
+            stair[i] = sc.nextInt();
+        }
+
+        if(K == 1){
+            System.out.println(stair[1]);
+        } else if(K==2){
+            System.out.println(stair[1] + stair[2]);
+        } else {
+            score[1] = stair[1];
+            score[2] = stair[1] + stair[2];
+            score[3] = Math.max(stair[1], stair[2]) + stair[3];
+            for(int i=4; i<=K; i++){
+                score[i] = Math.max(score[i-3] + stair[i-1], score[i-2])+ stair[i];
+            }
+            System.out.println(score[K]);
+        }
+    }
+}
