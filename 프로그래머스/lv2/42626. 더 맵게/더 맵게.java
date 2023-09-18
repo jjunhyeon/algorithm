dff5c981ecca8de9d73ed3bf233b3eee68853cef
김준현 <37209763+jjunhyeon@users.noreply.github.com>
2023-09-18 20:17:24 +0900

    [level 2] Title: 더 맵게, Time: 1406.17 ms, Memory: 123 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        
+        // array to PriorityQueue
+        PriorityQueue<Integer> mypq = new PriorityQueue<Integer>();   
+        for(int i=0; i<scoville.length; i++){  
+            mypq.offer(scoville[i]);            
+        }
+        
+        // pq에 담긴 값들을 앞의값부터 섞은후 넣는다.
+        // 가장 앞에 있는 값이 원하는 스코빌 지수보다 클떄까지
+        while((mypq.size() > 1) && mypq.peek()  < K){
+            // 1번 ,2번 값
+            int firstValue = mypq.poll();
+            int secondValue = mypq.poll();
+            // 두 값을 섞어야함
+            int newValue = firstValue + (secondValue * 2);
+            mypq.offer(newValue);
+            answer ++;
+        }
+        
+        return (mypq.peek() >= K) ? answer : -1;
+    }
+}
