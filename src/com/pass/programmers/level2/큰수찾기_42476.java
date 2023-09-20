e81129b4f2c9cc8d0ba73d1e836aa1f87748b84a
김준현 <sonjs01@naver.com>
2023-09-21 01:40:49 +0900

    큰수찾기 - 다시풀기(실패)

--- src/com/pass/programmers/level2/큰수찾기_42476.java
+++ src/com/pass/programmers/level2/큰수찾기_42476.java
@@ -3,7 +3,11 @@ import java.util.*;
 
 public class 큰수찾기_42476 {
     public static void main(String[] args) {
-        int[] numbers = {6, 10, 2};
+        int[] numbers = {12, 1213};
+        //int[] numbers = {1101, 1};
+        //int[] numbers = {110, 1};
+        //int[] numbers = {8, 9, 88898};
+        //int[] numbers = { 0,0,1};
         solution(numbers);
     }
 
@@ -21,6 +25,7 @@ public class 큰수찾기_42476 {
         }
 
         answer = sb.toString();
+        System.out.println("get answer ::" + answer);
         return answer;
     }
 
@@ -39,43 +44,44 @@ public class 큰수찾기_42476 {
 
         @Override
         public int compareTo(CustomBigNumber o) {
-
-            if (this.len >= 2) {
-                this.headValue = this.value / (int) Math.pow(10, this.len - 1);
-            }
-
-            if (o.len >= 2) {
-                o.headValue = o.value / (int) Math.pow(10, o.len - 1);
-            }
-
-            if (o.value == 0) {
-                // -1 -> 우선순위 뒤
+            // 0은 우선순위 가장 뒤
+            if (o.value == 0 ) {
                 return -1;
             }
+            // 초기 값, 길이에 따라 1보다 작으면 나머지로, 크면 몫으로 세팅
+            this.headValue = (this.len > 1) ? this.value / (int) Math.pow(10, this.len - 1) : this.value % (int) Math.pow(10, this.len);
+            o.headValue = (o.len > 1) ? o.value / (int) Math.pow(10, o.len - 1) : o.value % (int) Math.pow(10, o.len);
+
             // 그다음 자릿수와 비교를 해야함
             if (o.headValue == this.headValue) {
                 // headValue가 같은게 문제임, 정렬의 우선순위를 찾기 위함.
                 int oLen = (o.len > 1) ? o.len - 1 : 1;
                 int thisLen = (this.len > 1) ? this.len - 1 : 1;
-                while (o.headValue == this.headValue || (thisLen < 1 && oLen < 1)) {
+                
+                // 비교를 위한 value 정보
+                int tempValue = o.value;
+                int tempThisValue = this.value;
+                while (o.headValue == this.headValue ) {
                     if (oLen > 0) {
                         if (oLen == 1) {
-                            o.headValue = o.value % (int) Math.pow(10, oLen);
+                            o.headValue = tempValue % (int) Math.pow(10, oLen--);
                         } else {
-                            o.value = o.value % (int) Math.pow(10, oLen);
-                            oLen--;
-                            o.headValue = o.value / (int) Math.pow(10, oLen);
+                            tempValue = tempValue % (int) Math.pow(10, oLen--);
+                            o.headValue = tempValue / (int) Math.pow(10, oLen);
                         }
                     }
 
                     if (thisLen > 0) {
                         if (thisLen == 1) {
-                            this.headValue = this.value % (int) Math.pow(10, thisLen);
+                            this.headValue = tempThisValue % (int) Math.pow(10, thisLen--);
                         } else {
-                            this.headValue = this.value % (int) Math.pow(10, thisLen);
-                            thisLen--;
-                            this.headValue = this.value / (int) Math.pow(10, thisLen);
+                            tempThisValue = tempThisValue % (int) Math.pow(10, thisLen--);
+                            this.headValue = tempThisValue / (int) Math.pow(10, thisLen);
                         }
+                    }
+
+                    if(thisLen <= 0 && oLen <=0){
+                        break;
                     }
                 }
             }
