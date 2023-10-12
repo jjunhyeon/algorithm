e3ab29d40a0dab68224ee8864bb7f850bd2bdfb9
김준현 <37209763+jjunhyeon@users.noreply.github.com>
2023-10-12 11:27:13 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 3.64 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -0,0 +1,63 @@
+import java.util.*;
+class Solution {
+    Map<Integer,Map<String, Integer>> answerMap = new HashMap<>();// 길이, 메뉴조합, 반복횟수
+    Set<Integer> set = new HashSet<>(); // course
+    int dfsMaxValue;
+    public String[] solution(String[] orders, int[] course) {
+        
+        dfsMaxValue = course[course.length-1];
+        List<String> answerList = new ArrayList<>();
+        for(int n : course) answerMap.put(n, new HashMap<>());
+        for(int n : course) set.add(n);
+    
+        // 길이순으로 정렬
+        for(String order : orders){
+            char[] arr = order.toCharArray();
+            Arrays.sort(arr);
+            boolean[] visited = new boolean[order.length()];
+            setMenuCombinations(new String(arr), new StringBuilder(), 0, visited);    
+        }
+        
+        for(int n : course){
+            int max = 0; 
+            Map<String, Integer> temp = answerMap.get(n);
+            for(String key  : temp.keySet()){
+                max = Math.max(max,temp.get(key));   
+            }
+            
+            if(max < 2){ // 2미만 패스
+                continue;
+            } 
+            
+            for(String key : temp.keySet()){
+                if(temp.get(key) == max) answerList.add(key);
+            }
+            
+        }
+        Collections.sort(answerList);
+        return answerList.toArray(new String[0]);
+    }
+    
+    
+    
+    public  void setMenuCombinations(String order, StringBuilder stBuilder, int depth, boolean[] visited){
+        int len = stBuilder.length();
+        if(set.contains(len)){
+            String temp = stBuilder.toString();
+            int cnt = answerMap.get(len).getOrDefault(temp, 0) + 1;
+            answerMap.get(len).put(temp, cnt);
+            if(len == dfsMaxValue) return;
+        }
+        
+        for(int j=depth; j<order.length(); j++){
+          if(!visited[j]){
+              visited[j] = true;
+              stBuilder.append(order.charAt(j));
+              setMenuCombinations(order, stBuilder, j + 1, visited);
+              visited[j] = false;
+              stBuilder.deleteCharAt(stBuilder.length()-1);
+            }
+        }
+    }
+    
+}
