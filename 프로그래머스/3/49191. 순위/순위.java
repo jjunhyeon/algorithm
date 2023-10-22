694a2f69f8144c84134bb74b62668b7fa1a96afe
김준현 <37209763+jjunhyeon@users.noreply.github.com>
2023-10-22 15:49:15 +0900

    [level 3] Title: 순위, Time: 24.86 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -0,0 +1,11 @@
+
+// 14 :30 ~ 시작
+// 권투 경기는 1대 1방식으로 진행
+// 만약 A선수가 B선수보다 실력이 좋다면 A선수는 항상 B선수를 이김.
+// 주어진 경기 결과로 순위를 매긴다.
+class Solution {
+    public int solution(int n, int[][] results) {
+        int answer = 0;
+        return answer;
+    }
+}
