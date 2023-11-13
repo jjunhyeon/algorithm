16097630b5ef68d76f6861abab2e215af368bad9
김준현 <37209763+jjunhyeon@users.noreply.github.com>
2023-11-13 12:24:53 +0900

    [level 3] Title: 양과 늑대, Time: 1.24 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,6 +1,50 @@
+import java.util.*;
 class Solution {
+    static ArrayList<ArrayList<Integer>> nodes = new ArrayList<ArrayList<Integer>>();
+    static int answer = Integer.MIN_VALUE;
     public int solution(int[] info, int[][] edges) {
-        int answer = 0;
+        
+        int nodeCount = info.length;
+        for(int i=0; i<nodeCount; i++){
+            nodes.add(new ArrayList<Integer>());
+        }
+        
+        int edgeCount = edges.length;
+        for(int i=0; i<edgeCount; i++){
+            int[] edge = edges[i];
+            nodes.get(edge[0]).add(edge[1]);
+        }
+                
+
+        dfs(0,0,0,new ArrayList<Integer>(),info);
         return answer;
+    }
+    
+    public static void dfs(int curNode, int sheepCount, int wolfCount, ArrayList<Integer> nextNodeList,int[] info){
+        
+        if(info[curNode] == 0){
+            sheepCount += 1;
+        } else {
+            wolfCount +=1;
+        }
+        
+        if(sheepCount <= wolfCount){
+            return;
+        }
+        
+        answer = Math.max(answer, sheepCount);
+
+        // 다음 탐색 위치 갱신 -> 자기 자신 제외 해야함
+        ArrayList<Integer> nextLocation = new ArrayList<Integer>(nextNodeList);
+        
+        if(!nodes.get(curNode).isEmpty()){
+            nextLocation.addAll(nodes.get(curNode));
+        }
+        nextLocation.remove(Integer.valueOf(curNode));
+        
+        for(int cur : nextLocation){
+            dfs(cur, sheepCount, wolfCount, nextLocation,info);
+        }
+
     }
 }
