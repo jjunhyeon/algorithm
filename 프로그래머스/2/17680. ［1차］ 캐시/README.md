1ecc934535711344881a3184fa41e603ae69e7b1
김준현 <37209763+jjunhyeon@users.noreply.github.com>
2023-11-13 15:13:40 +0900

    [level 2] Title: [1차] 캐시, Time: 4.35 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/README.md
+++ 프로그래머스/2/17680. ［1차］ 캐시/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 캐시 - 17680 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680#) 
 
 ### 성능 요약
 
-메모리: 87.8 MB, 시간: 12.09 ms
+메모리: 78 MB, 시간: 4.35 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 1일 15:6:6
+2023년 11월 1일 15:13:38
 
 ### 문제 설명
 
--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -2,26 +2,25 @@ import java.util.*;
 
 class Solution {
     public int solution(int cacheSize, String[] cities) {
-        int result =0;
-        cities = Arrays.stream(cities)
-                .map(String::toUpperCase)
-                .toArray(String[]::new);
-
+                int result =0;
+        cities = Arrays.stream(cities).map(String::toUpperCase).toArray(String[]:: new);
         Deque<String> myDeque = new LinkedList<>();
         for(String city : cities){
-            
+            if(cacheSize == 0){
+                result += cities.length * 5;
+                break;
+            }
             // 캐시 히트임
             if(myDeque.contains(city)){
                 myDeque.remove(city);
                 result += 1;
             } else {
+
                 result += 5;
             }
-            if(cacheSize ==0) continue;
-            
             if(myDeque.size() + 1 > cacheSize){
                 myDeque.poll();
-            }  
+            }
             myDeque.offer(city);
         }
         return result;
