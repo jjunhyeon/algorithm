e792ae91e6adc9a7593eeeec035f2f3db517ab66
김준현 <sonjs01@naver.com>
2024-01-14 21:13:59 +0900

    [Silver III] Title: 계단 오르기, Time: 80 ms, Memory: 11584 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,28 +1,33 @@
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+// 백준 - 계단오르기
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int K = sc.nextInt();
-        int[] stair = new int[K+1];
-        int[] score = new int[K+1];
-        for(int i=1; i<=K; i++){
-            stair[i] = sc.nextInt();
-        }
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        if(K == 1){
-            System.out.println(stair[1]);
-        } else if(K==2){
-            System.out.println(stair[1] + stair[2]);
-        } else {
-            score[1] = stair[1];
-            score[2] = stair[1] + stair[2];
-            score[3] = Math.max(stair[1], stair[2]) + stair[3];
-            for(int i=4; i<=K; i++){
-                score[i] = Math.max(score[i-3] + stair[i-1], score[i-2])+ stair[i];
+        int N = Integer.parseInt(bf.readLine());
+        // 계단
+        int[] stair = new int[N + 1];
+        // 현재 계단 값의 최대값을 저장할 DP 배열 생성
+        int[] dp = new int[N + 1];
+        for (int i = 1; i <= N; i++) stair[i] = Integer.parseInt(bf.readLine());
+        // 입력 끝
+        bf.close();
+        // 시작
+        dp[1] = stair[1];
+        if (N >= 3) {
+            //한번에 2칸 vs (1칸 + 1칸)
+            dp[2] = Math.max(stair[2], stair[1] + stair[2]);
+            // 1칸 + 3칸 vs 2칸 + 3칸
+            dp[3] = Math.max(stair[1] + stair[3], stair[2] + stair[3]);
+            for (int i = 3; i <= N; i++) {
+                // 전전칸까지의 최대값 + 현재칸 VS 전전전칸까지의 최대값 + 전칸 + 현재
+                dp[i] = Math.max(stair[i] + dp[i - 2], stair[i] + dp[i - 3] + stair[i - 1]);
             }
-            System.out.println(score[K]);
         }
+        System.out.println(N == 2 ? Math.max(stair[2], stair[1] + stair[2]) : dp[N]);
     }
 }
