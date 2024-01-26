8f17c0a863368869dde528d587aa0e8ae45b2afe
김준현 <sonjs01@naver.com>
2024-01-26 18:18:14 +0900

    배낭만들기 - not yet

---
+++ src/com/pass/boj/dp/Boh_배낭만들기_12865.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// 백준 - 배낭 만들기
+public class Boh_배낭만들기_12865 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st;
+		st = new StringTokenizer(bf.readLine());
+		
+		// Test case와 무게 변수 값
+		int T = Integer.parseInt(st.nextToken());
+		int W = Integer.parseInt(st.nextToken());
+
+		// 무게별, 최대 가중치 정보를 저장한다.
+		int[] dp = new int[W+1];
+		
+		while(T --> 0) {
+			st = new StringTokenizer(bf.readLine());
+			int weight = Integer.parseInt(st.nextToken());
+			int value = Integer.parseInt(st.nextToken());
+			
+			// 무게별, 최대 가중치 값으로 저장한다.
+			dp[weight] = Math.max(dp[weight], value);
+			
+			// TO DO DFS로 더하기 케이스 구분 후 처리해야할듯..?
+		}
+		
+	}
+}
