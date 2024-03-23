3769eb5b30f9ab615479e4c743c934ca820f16a6
sonjs01@naver.com <sonjs01@naver.com>
2024-03-23 19:31:30 +0900

    어렵 - 다시 풀기

---
+++ src/com/pass/boj/study/책페이지_1019.java
@@ -0,0 +1,51 @@
+package com.pass.boj.study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 책페이지_1019 {
+    static int[] cnt = new int[10];
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int targetNum = Integer.parseInt(bf.readLine());
+
+        int start = 1;
+        int digit = 1;
+        // 초기값보다 입력값이 작아질대까지
+        while(start <= targetNum){
+
+            while(start % 10 != 0 && start <= targetNum){
+                count(start,digit);
+                start ++;
+            }
+
+            while(targetNum % 10 != 9 && start <= targetNum){
+                count(targetNum, digit);
+                targetNum--;
+            }
+
+            if(start > targetNum) break;
+
+            // 마지막 자릿수를 제거한다.
+            start /= 10;
+            targetNum /= 10;
+
+            for(int i=0; i<10; i++){
+                cnt[i] += (targetNum - start +1) * digit;
+            }
+
+            digit = digit * 10;
+        }
+
+        for(long c : cnt) System.out.print(c + " ");
+    }
+
+    private static void count(int n, int digit) {
+        while(n > 0){
+            // cnt 배열에 값 적재
+            cnt[n % 10] += digit;
+            n = n / 10;
+        }
+    }
+}
