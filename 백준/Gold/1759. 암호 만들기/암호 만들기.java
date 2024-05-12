24d67cfe2069c06a3bc8298ab2ab2d78bfa2277e
김준현 <sonjs01@naver.com>
2024-05-12 23:22:20 +0900

    [Gold V] Title: 암호 만들기, Time: 88 ms, Memory: 11808 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -0,0 +1,82 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * packageName    : com.pass.boj.study
+ * fileName       : 암호만들기_1759
+ * author         : junhyeon
+ * date           : 2024-05-12
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-05-12        junhyeon       최초 생성
+ */
+public class Main {
+    static int len, fullLen;
+    static String[] word;
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        len = Integer.parseInt(st.nextToken());
+        fullLen = Integer.parseInt(st.nextToken());
+        word = new String[fullLen];
+
+        st = new StringTokenizer(bf.readLine());
+        for(int i=0; i<fullLen; i++){
+            word[i] = st.nextToken();
+        }
+
+        // 사전순 정렬
+        Arrays.sort(word);
+        boolean[] visited =new boolean[fullLen];
+        dfsByWord(0, 0, visited, new StringBuilder());
+    }
+
+    private static void dfsByWord(int start, int depth, boolean[] visited, StringBuilder cur) {
+
+        if(depth == len){
+            if(checkContainAEIOU(cur) && checkAtLeastTwoCharNotAEIOU(cur)){
+                System.out.println(cur);
+            }
+            return;
+        }
+
+        for(int i=start; i<fullLen; i++){
+            if(!visited[i]){
+                cur.append(word[i]);
+                visited[i] = true;
+                dfsByWord(i+1, depth+1, visited, cur);
+                visited[i] = false;
+                cur.setLength(cur.length() - 1);
+            }
+        }
+    }
+
+    private static boolean checkContainAEIOU(StringBuilder st){
+        for(char ch : st.toString().toCharArray()){
+            if((ch == 'a' || ch == 'e' || ch =='i' || ch =='o' || ch == 'u')){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean checkAtLeastTwoCharNotAEIOU(StringBuilder st){
+        int check = 0;
+        for(char ch : st.toString().toCharArray()){
+            if(!(ch == 'a' || ch == 'e' || ch =='i' || ch =='o' || ch == 'u')){
+                check ++;
+            }
+            if(check >= 2){
+                return true;
+            }
+        }
+        return false;
+    }
+}
