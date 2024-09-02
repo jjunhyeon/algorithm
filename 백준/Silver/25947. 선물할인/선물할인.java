aece96fda593968c7d28315a6c19d690407ff084
김준현 <sonjs01@naver.com>
2024-09-02 20:43:53 +0900

    [Silver I] Title: 선물할인, Time: 432 ms, Memory: 32628 KB -BaekjoonHub

--- 백준/Silver/25947. 선물할인/선물할인.java
+++ 백준/Silver/25947. 선물할인/선물할인.java
@@ -1,55 +1,54 @@
-
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-// 그리디 - 선물할린
 public class Main {
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		// 선물의개수,예산,할인 가능 선물의 수
 		int PRESENT_COUNT = Integer.parseInt(st.nextToken());
 		int BUDGET = Integer.parseInt(st.nextToken());
 		int DISCOUNT_COUNT = Integer.parseInt(st.nextToken());
 
-		int[] preSent = new int[PRESENT_COUNT];
+		int[] array = new int[PRESENT_COUNT];
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < PRESENT_COUNT; i++) {
-			preSent[i] = Integer.parseInt(st.nextToken());
+			array[i] = Integer.parseInt(st.nextToken());
 		}
-		// 순차적으로 값을 누적시키기 위해 정렬한다.
-		Arrays.sort(preSent);
+		// 선물에 대한 값을 정렬한다.
+		Arrays.sort(array);
+
+		// 정렬 후 start, end 지점을 잡고 앞에서 부터 탐색한다.
+		// DISCOUNT 정보가 있는거에 대한 IF를 구분하고
+		// 있을경우 sum에 할인 갑을 누적시키고
+		// 만약 DISOCUNT가 더이상 존재하지 않을경우 START와 END지점을 조절하며 최종 확정한다.
 		int sum = 0;
-		// 모든 품목에 대해 할인 가능한 케이스
 		int start = 0;
 		int end = 0;
-
 		for (int i = 0; i < PRESENT_COUNT; i++) {
+			// 할인가능
 			if (DISCOUNT_COUNT > 0) {
-				// BUDGET을 초과한시에서 break;
-				if (sum + preSent[i] / 2 > BUDGET) {
+				if (sum + array[i] / 2 > BUDGET) {
 					break;
 				} else {
-					sum += preSent[i] / 2;
+					sum += array[i] / 2;
 					DISCOUNT_COUNT--;
 					end = i + 1;
 				}
-			} else { // 할인불가, 할인 카운트 전부 사용
-				// 합계 적용
-				// 할인품목 시자지점과 끝지점을 조절해주는 부분
-				sum += preSent[start] / 2;
-				sum += preSent[end] / 2;
+			} else { // 할인불가
+				sum += array[start] / 2;
+				sum += array[end] / 2;
 				if (sum > BUDGET) {
 					break;
-				} else {
+				} else { // end를 더 확보할수 있으면
 					start++;
 					end++;
 				}
 			}
-
 		}
+
 		System.out.println(end);
 	}
 }
