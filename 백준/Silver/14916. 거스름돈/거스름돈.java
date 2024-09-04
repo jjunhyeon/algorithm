e67488c3c2761fbcda421c435ddc6aedc61a4f29
김준현 <sonjs01@naver.com>
2024-09-04 21:02:49 +0900

    [Silver V] Title: 거스름돈, Time: 96 ms, Memory: 12840 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.java
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -1,30 +1,41 @@
-import java.util.Arrays;
+
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int K = sc.nextInt();
-        int[] dp = new int[100001];
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		// 거스름돈
+		int M = sc.nextInt();
+		int[] coin = new int[6];
+		// 5원 또는 2원으로만 남겨주어야한다.
+		// 1. 먼저 5원으로 나눈 몫을 result ++
+		// 13원일경우에는??
+		// 2.
+		// 케이스를 세분화해 만약 홀수일경우, 몫보다 -1만큼을 적재하고 차액에 +5를 해서 2원으로 나누어 지도록 한다.
+		// 그래도 나누어지지 않을경우 -1을 출력한다.
 
-        Arrays.fill(dp,Integer.MAX_VALUE);
-
-        dp[1] = -1;
-        dp[2] = 1;
-        dp[3] = -1;
-        dp[4] = 2;
-        dp[5] = 1;
-
-        for(int i=6;i <=K; i++){
-            if(dp[i-2] == -1){
-                dp[i] = dp[i-5] + 1;
-            } else if(dp[i-5] == -1){
-                dp[i] = dp[i-2] + 1;
-            } else {
-                dp[i] = Math.min(dp[i-5],dp[i-2]) + 1;
-            }
-        }
-
-        System.out.println(dp[K]);
-    }
+		while (M > 0) {
+			// 홀수라면
+			if (M < 5) {
+				if (M % 2 == 1) {
+					if(coin[5] > 0) {
+						coin[5]--;
+						coin[2] += (M + 5)/ 2;	
+					} else {
+						// cant
+						coin[2]= -1;
+					}
+					break;
+				} else {
+					coin[2] += M / 2;
+					break;
+				}
+			}
+			// 금액이 짝수일경우
+			coin[5] += M / 5;
+			// 나머지를 M으로 적재
+			M = M % 5;
+		}
+		System.out.println(coin[2] == -1 ? -1 : coin[2] + coin[5]);
+	}
 }
