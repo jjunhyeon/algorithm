be930d6bc89fa9e7e761887078206bdf9a67175f
김준현 <sonjs01@naver.com>
2025-01-01 20:57:24 +0900

    2025 시작!

---
+++ 2025/src/algorithm/Test.java
@@ -0,0 +1,9 @@
+package algorithm;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("2025 Start !!");
+		System.out.println("Solve Algorithm Every day!");
+		System.out.println("Fighthing!!");
+	}
+}
