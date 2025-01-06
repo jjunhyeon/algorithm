9a8c1cb99630ebd3981c38f7a33fcd8f35abb093
김준현 <sonjs01@naver.com>
2025-01-06 23:18:59 +0900

    message update

--- 2025/algorithm/Test.java
+++ 2025/algorithm/Test.java
@@ -2,8 +2,6 @@ package algorithm;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("2025 Start !!");
-		System.out.println("Solve Algorithm Every day!");
-		System.out.println("Fighthing!!");
+		System.out.println("2025 Start Fighthing!!");
 	}
 }
--- 2025/month1/Test.java
+++
@@ -1,8 +0,0 @@
-package month1;
-
-public class Test {
-	public static void main(String[] args) {
-		System.out.println("Monyh1 !!");
-		System.out.println("Fighthing!!");
-	}
-}
