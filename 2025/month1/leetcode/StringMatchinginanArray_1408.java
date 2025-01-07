5018ae75de3b39ac76c37dbb8ce66eeb23a88905
김준현 <sonjs01@naver.com>
2025-01-07 23:34:58 +0900

    leetcode start

---
+++ 2025/month1/leetcode/StringMatchinginanArray_1408.java
@@ -0,0 +1,27 @@
+package month1.leetcode;
+
+
+/**
+ * Given an array of string words, return all strings in words that is a substring of another word. You can return the answer in any order.
+A substring is a contiguous sequence of characters within a string
+
+Example 1:
+Input: words = ["mass","as","hero","superhero"]
+Output: ["as","hero"]
+Explanation: "as" is substring of "mass" and "hero" is substring of "superhero".
+["hero","as"] is also a valid answer.
+Example 2:
+
+Input: words = ["leetcode","et","code"]
+Output: ["et","code"]
+Explanation: "et", "code" are substring of "leetcode".
+Example 3:
+
+Input: words = ["blue","green","bu"]
+Output: []
+Explanation: No string of words is substring of another string.
+Start date : 1/08
+ */
+public class StringMatchinginanArray_1408 {
+
+}
