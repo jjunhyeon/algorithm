b250cdb8b740a8c8c604772761ec88808df2a826
김준현 <sonjs01@naver.com>
2025-03-04 13:14:51 +0900

    today leetcode add

---
+++ 2025/month3/Lc_MergeAndArray.java
@@ -0,0 +1,25 @@
+package month3;
+
+/*
+ * https://leetcode.com/problems/merge-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
+ * 정렬된 수 반환
+ * 2025-03-04 today (12: 30 ~ 12: 55)
+ * 문제파악
+*/
+public class Lc_MergeAndArray {
+	public static void main(String[] args) {
+		// example data
+		int[] nums1 = {1,2,3,0,0,0};
+		int num1 = 3;
+		int[] nums2 = {2,5,6};
+		int num2 = 3;
+		solution(nums1, num1, nums2, num2);
+	}
+
+	// return : merged array1
+	// @note : the final sorted array should not be returned by the function
+	// but instead be stored inside the array nums1.
+	private static void solution(int[] nums1, int num1, int[] nums2, int num2) {
+		
+	}
+}
