79acc7e4f5d8a120e6a1656f8afcd9456433e73e
김준현 <sonjs01@naver.com>
2025-03-19 13:03:08 +0900

    통과했지만, HashMap을 활용해 시간복잡도 개선 필요

---
+++ 2025/month3/LC_TwoSum.java
@@ -0,0 +1,33 @@
+package month3;
+
+import java.util.Arrays;
+
+/*
+ * twom sum
+ * link : https://leetcode.com/problems/two-sum/description/
+ * O(N^2) 이하로 구현한다.
+*/
+public class LC_TwoSum {
+	public static void main(String[] args) {
+		int[] nums = { 3, 3 };
+		int target = 6;
+		System.out.println("Arrys::: >>" + Arrays.toString(solution(nums, target)));
+	}
+
+	private static int[] solution(int[] nums, int target) {
+		int start = 0;
+		int len = nums.length;
+		while (start < len) {
+			for (int i = 0; i < len; i++) {
+				if (start == i)
+					continue;
+
+				if (nums[i] + nums[start] == target) {
+					return new int[] { start, i };
+				}
+			}
+			start++;
+		}
+		return null;
+	}
+}
