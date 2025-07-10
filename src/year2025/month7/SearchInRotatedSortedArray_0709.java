eb7b71dcf4331f68c3a6e65f54e4a8fc00eaa2e7
김준현 <sonjs01@naver.com>
2025-07-10 22:10:34 +0900

    회전정렬에서 target 찾기 - 이분탐색

--- src/year2025/month7/SearchInRotatedSortedArray_0709.java
+++ src/year2025/month7/SearchInRotatedSortedArray_0709.java
@@ -2,12 +2,14 @@ package year2025.month7;
 
 /*
 * 중복값이 존재하지 않는 오름차순 정렬된 배열에서 
-* 타겟 정보의 값이 존재하면 인덱스를 반환하고 없으면 -1을 리턴해라. 
+* 타겟 정보의 값이 존재하면 인덱스를 반환하고 없으면 -1을 리턴해라.
+* 0709 실패
+* 0710 재도전 
 */
 public class SearchInRotatedSortedArray_0709 {
 	public static void main(String[] args) {
 		int[] num = new int[] {1,3};
-		System.out.println(":::" + solution(num, 3));
+		solution(num, 3);
 	}
 
 	private static int solution(int[] num, int target) {
@@ -15,45 +17,25 @@ public class SearchInRotatedSortedArray_0709 {
 		int left =0;
 		int right = num.length -1;
 		
-		while(left < right) {
+		while(left <= right) {
 			int mid = (left + right) / 2;
 			// 내가 찾는값보다 num[mid] 정보가 우측에 있으면 
-			if(num[right] < num[mid]) {
-				left = mid + 1;
-			} else {
-				right = mid;
-			}
-		}
-		
-		if(target == num[left]) {
-			return left;
-		}
-		
-		// left가 다시 left인 지점
-		int findLeft = 0;
-		int findRight = 0;
-		if(target > num[left]) {
-			findLeft = left;
-			findRight = num.length-1;
-		} else {
-			findLeft = 0;
-			findRight = left;
-		}
-	
-		while(findLeft < findRight) {
-			int mid = (findLeft + findRight) / 2;
-			// 내가 찾는값보다 num[mid] 정보가 우측에 있으면 
-			if(target == num[mid]) {
-				return mid;
-			}
+			if(num[mid] == target) return mid;
 			
-		    if (num[mid] < target) {
-		        findLeft = mid + 1;
-		    } else {
-		        findRight = mid - 1;
-		    }
+			if(num[left] <= num[mid]) { // 왼쪽이 정렬상태
+				if(num[left] <= target && target < num[mid]) {
+					right = mid - 1;
+				} else {
+					left = mid +1;
+				}
+			} else { // 오른쪽 정렬상태
+				if(num[mid] < target && target <= num[right]) {
+					left = mid + 1;
+				} else {
+					right = mid -1;
+				}
+			}
 		}
-
 		return -1;
 	}
 }
