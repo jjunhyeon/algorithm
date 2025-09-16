35e612088fc081739587b604e087c23379ea82b0
김준현 <sonjs01@naver.com>
2025-09-16 13:03:26 +0900

    문제해결 - bfs

--- src/year2025/moth9/LC_SurroundedRegions.java
+++ src/year2025/moth9/LC_SurroundedRegions.java
@@ -14,24 +14,22 @@ public class LC_SurroundedRegions {
 	public static int row, col;
 	public static int[] xRange = {-1,0,1,0};
 	public static int[] yRange = {0,1,0,-1};
-	public static char[][] copied;
+	public static char[][] board;
 	
 	public static void main(String[] args) {
-	   char[][] board = {{'X','X','X','X'},{'X','O','O','X'},{'X','X','O','X'},{'X','O','X','X'}};
+		char [][] board = {{'O','O','O'},{'O','O','O'},{'O','O','O'}};
+	   // char[][] board = {{'X','X','X','X'},{'X','O','O','X'},{'X','X','O','X'},{'X','O','X','X'}};
 	   solution(board);
 	}
 
-	private static void solution(char[][] board) {
+	private static void solution(char[][] inputBoard) {
 		// 이차원배열을 탐색하며 O인 지점을 만나면 주변을 찾아나가는 BFS 탐색을 시작한다.
 		// Quee에 담아 탐색하며 만약 EDGE에 인접한 지점일 경우 해당 영역은 그대로 O로 남기며 
 		// EDGE와 인접한 영역이 존재하지 않을경우 해당 영역을 모두 X로 바꿔버린다.
+		board = inputBoard;
 		row = board.length;
 		col = board[0].length;
 		visited = new boolean[row][col];
-		copied = new char[row][col];
-		for(int i=0; i<board.length; i++) {
-			copied[i] = board[i].clone();
-		}
 		
 		// 꼭지점 중복에 대해 고민
 		for(int i=0; i < col; i++) {
@@ -61,7 +59,6 @@ public class LC_SurroundedRegions {
 			}
 		}
 		
-		
 		for(int i=0; i<row; i++) {
 			for(int j=0; j<col; j++) {
 				if(board[i][j] == 'O' && !visited[i][j])  {
@@ -83,12 +80,9 @@ public class LC_SurroundedRegions {
 				int nextX = cur[0] + xRange[i];
 				int nextY = cur[1] + yRange[i];
 				// 정상범위이면서 O 이면서 방문하지 않았다면
-				if(nextX >= 0 && nextX < row && nextY >= 0 && nextY < col && !visited[nextX][nextY] && copied[nextX][nextY] == 'O') {
-					if(copied[x][y] != 'T'){
-						copied[x][y] = 'T';
-					}
+				if(nextX >= 0 && nextX < row && nextY >= 0 && nextY < col && !visited[nextX][nextY] && board[nextX][nextY] == 'O') {
+					target.add(new int[] {nextX,nextY});
 					visited[nextX][nextY] = true;
-					copied[nextX][nextY] = 'T';
 				}
 			}
 		}
